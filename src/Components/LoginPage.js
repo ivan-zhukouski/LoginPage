3d6eb39adc7a681ed3e220932d0cf964b2f95b52
VanoVeryWonderful <stigmatix.by@gmail.com>
2019-08-08 00:34:53 +0300

    done the form

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -5,41 +5,44 @@ import '../form.css'
 const src= '../Images/Logo.png';
 export default class LogoPage extends Component{
     constructor(props){
-        super(props)
+        super(props);
         this.state={
             email: '',
             password:'',
-            submittedPassword:'',
             submittedEmail:'',
+            submittedPassword:''
         }
     }
     handleChange = event => this.setState({[event.target.id]:event.target.value});
     handleSubmit = (event)=> {
+        const {email, password} = this.state;
+        this.setState({submittedEmail: email,submittedPassword:password});
+        console.log('Submitted email is ' + this.state.submittedEmail + ' and password is '+ this.state.submittedPassword)
         event.preventDefault();
-        this.setState({submittedEmail: event.target.value,submittedPassword:event.target.value})
     };
     render(){
-        console.log(this.state);
         return(
             <div>
                 <div className='img_block'>
                     <Image className='logo_img' src={src} size='tiny' circular verticalAlign='middle'/><span>Log-in to your account</span>
                 </div>
-                <Form className = 'main_form' onSubmit={this.handleSubmit}>
+                <Form className = 'main_form' onSubmit={this.handleSubmit.bind(this)}>
                     <Form.Field >
                         <Form.Input icon='user'
                                     iconPosition='left'
                                     placeholder='E-mail'
                                     name='e-mail'
                                     value={this.state.email}
-                                    onChange={this.handleChange.bind(this)} id='email' />
+                                    onChange={this.handleChange.bind(this)}
+                                    id='email' />
                         <Form.Input icon='lock'
                                     iconPosition='left'
                                     placeholder='Password'
                                     type='password'
                                     name='password'
                                     value={this.state.password}
-                                    onChange={this.handleChange.bind(this)} id='password'/>
+                                    onChange={this.handleChange.bind(this)}
+                                    id='password'/>
                     </Form.Field >
                     <Form.Button fluid color='teal' content='Submit'/>
                 </Form>
