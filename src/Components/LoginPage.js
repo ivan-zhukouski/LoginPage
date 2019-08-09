5d19115dc939c71c900c9368322b8eb81fb1f45d
VanoVeryWonderful <stigmatix.by@gmail.com>
2019-08-10 01:20:29 +0300

    almost done submit

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import { Form,Image } from 'semantic-ui-react'
+import {Form, Image, Message} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import '../form.css'
 const src= '../Images/Logo.png';
@@ -10,35 +10,45 @@ export default class LogoPage extends Component{
             email: '',
             password:'',
             submittedEmail:'',
-            submittedPassword:''
-        }
+            submittedPassword:'',
+            loading: false,
+            errorForm: false,
+            errorInput: false,
+
+        };
+        this.handleOnSubmit=this.handleOnSubmit.bind(this);
+        this.handleChange=this.handleChange.bind(this)
     }
     handleChange = event => this.setState({[event.target.id]:event.target.value});
-    handleSubmit (event){
+    handleOnSubmit (event){
         event.preventDefault();
-        const {email, password} = this.state;
-        this.setState({submittedEmail: email,submittedPassword:password});
-        console.log('Submitted email is ' + this.state.submittedEmail + ' and password is '+ this.state.submittedPassword);
-        // if (this.state.submittedEmail === 'stig' , this.state.submittedPassword === 'stig'){
-        //    alert('Hi Stig');
+        console.log('Submitted email is ' + this.state.email +
+            ' and password is '+ this.state.password);
+        if (this.state.email === 'stig' , this.state.password === 'stig'){
+             alert('Hi Stig');
+          }
+        // if(!this.state.email && !this.state.password){
+        //     this.setState({errorForm:true,errorInput:true})
         // }
-        // else{
-        //     this.props.history.push({
-        //         pathname: '/error'
-        //     })
-        // }
-    };
+    }
     render(){
         return(
             <div className='container'>
                 <div className='img_block'>
-                    <Image className='logo_img' src={src} circular verticalAlign='middle'/>
+                    <Image className='logo_img'
+                           src={src}
+                           circular
+                           verticalAlign='middle'/>
                     <span className='img_text'>Log-in to your account</span>
 
                 </div>
-                <Form className = 'main_form' onSubmit={this.handleSubmit.bind(this)}>
+                <Form error={this.state.errorForm}
+                      className = 'main_form'
+                      >
                     <Form.Field >
-                        <Form.Input transparent size='big'
+                        <Form.Input error={this.state.errorInput}
+                                    transparent
+                                    size='big'
                                     icon='user'
                                     iconPosition='left'
                                     placeholder='E-mail'
@@ -46,16 +56,27 @@ export default class LogoPage extends Component{
                                     value={this.state.email}
                                     onChange={this.handleChange.bind(this)}
                                     id='email' />
-                        <Form.Input transparent size='big' icon='lock'
+                        <Form.Input error={this.state.errorInput}
+                                    transparent
+                                    size='big'
+                                    icon='lock'
                                     iconPosition='left'
                                     placeholder='Password'
                                     type='password'
                                     name='password'
                                     value={this.state.password}
-                                    onChange={this.handleChange.bind(this)}
+                                    onChange={this.handleChange}
                                     id='password'/>
+                        <Message
+                            error
+                            header='Error'
+                            content='The E-mail address or password is incorrect'
+                        />
                     </Form.Field >
-                    <Form.Button fluid color='teal' content='Submit'/>
+                    <Form.Button loading={this.state.loading}
+                                 fluid
+                                 color='teal'
+                                 content='Submit' onClick={this.handleOnSubmit}/>
                 </Form>
             </div>
         )
